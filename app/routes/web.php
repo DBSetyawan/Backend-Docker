f4b4fb225d2fb2afceb5a433504bb124722531d6
Daniel Setyawan <artexsdns@gmail.com>
2022-07-19 20:38:31 +0700

    Fill oauth create or update data API.

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,12 +1,55 @@
 <?php
 
 use App\Models\User;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json('API ACI Fullstack developer depends on Docker In system administration, orchestration is the automated configuration, coordination, and management of computer systems and software.');
+    return response()->json(['iamJSON' => 'API ACI Fullstack developer depends on Docker In system administration, orchestration is the automated configuration, coordination, and management of computer systems and software.']);
 });
 
-Route::get('/test-json', function () {
-    return response()->json(['sdasd' => User::all()]);
+Route::get('/oauth-aci', function (Request $request) {
+
+    $client = new Client([
+        'headers' => ['Content-Type' => 'application/json']
+    ]);
+
+    $response = $client->post(
+        'https://devel.bebasbayar.com/web/test_programmer.php',
+        ['body' => json_encode(
+            [
+                'user' => $request->user,
+                'password' => $request->password
+            ]
+        )]
+    );
+
+    $data = json_decode($response->getBody()->getContents(), true);
+
+    if ($data['rd'] == "Sukses") {
+
+        $auth = ['user' => $request->user, 'password' => $request->password];
+
+        $data = User::updateOrCreate($auth, ['user' => $request->user, 'password' => $request->password]);
+
+        $data = User::first();
+        /**
+         * if data successfully insert, but not update
+         * 
+         * {
+                "response": {
+                    "user": "admin",
+                    "updated_at": "2022-07-19T13:34:49.000000Z",
+                    "created_at": "2022-07-19T13:34:49.000000Z",
+                    "id": 1
+                }
+            }
+         */
+    } else {
+
+        $data = $data;
+    }
+
+    return response()->json(['response' => $data]);
 });
